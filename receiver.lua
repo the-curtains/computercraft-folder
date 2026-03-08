357f2c8f9893a56575f29382b8a0e1ea5c1293be
jaden-tan-np <S10268361@connect.np.edu.sg>
2026-03-08 09:56:42 +0800

    2

--- receiver.lua
+++ receiver.lua
@@ -1,14 +1,15 @@
+local hostname = ""
 if io.open("config.dat") == nil then
 	print("looks like this is your config phase. exciting!")
 	print("what do you wanna name this appliance!?")
-	local hostname = io.read()
+	hostname = io.read()
 	print("cool!")
 	local file = io.open("config.dat", "w")
 	file:write(hostname)
 	file:close()
 else
 	local file = io.open("config.dat", "r")
-	local hostname = file:read("*a")
+	hostname = file:read("*a")
 end
 
 rednet.open("right")
--- sender.lua
+++ sender.lua
@@ -2,7 +2,7 @@ rednet.open("right")
 
 while true do
 	io.write("hi there mr man do you wanna send a msg:")
-	local name = io.read()
+	local name = tostring(io.read())
 	local id = rednet.lookup("receiver", name)
 	if not id then
 		print("please put in a hostname that exists fucker")
