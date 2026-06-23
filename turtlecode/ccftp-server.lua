d033d81be403fd20137c7ae67fd4f426402b49a2
jaden-tan-np <S10268361@connect.np.edu.sg>
2026-06-23 17:36:36 +0800

    a

--- turtlecode/ccftp-server.lua
+++ turtlecode/ccftp-server.lua
@@ -1,44 +1,44 @@
-    --server
+--server
 
-    local hostname = ""
+local hostname = ""
 
-    if io.open("config.dat") == nil then
-        print("looks like this is your config phase. exciting!")
-        print("what do you wanna name this server!?")
-        hostname = io.read()
-        print("cool!")
-        local file = io.open("config.dat", "w")
-        file:write(hostname)
-        file:close()
-    else
-        local file = io.open("config.dat", "r")
-        hostname = file:read("*a")
+if io.open("config.dat") == nil then
+    print("looks like this is your config phase. exciting!")
+    print("what do you wanna name this server!?")
+    hostname = io.read()
+    print("cool!")
+    local file = io.open("config.dat", "w")
+    file:write(hostname)
+    file:close()
+else
+    local file = io.open("config.dat", "r")
+    hostname = file:read("*a")
+end
+
+local function awaitFile()
+    local _, msg = rednet.receive()
+    return msg
+end
+
+
+
+rednet.open("right")
+rednet.host("ccftp", hostname)
+
+
+
+while true do
+    local file = textutils.unserialise(awaitFile())
+    local filename = file["filename"]
+    local fileTable = file["file"]
+    local write = ""
+    for _, line in ipairs(fileTable) do
+        write = write .. line .. "\n"
     end
-
-    local function awaitFile()
-        local _, msg = rednet.receive()
-        return msg
+    write = string.sub(write, 1, #write - 1)
+    local writeFile = io.open(filename, "w")
+    if writeFile then    
+        writeFile:write(write)
+        writeFile:close()
     end
-
-
-
-    rednet.open("right")
-    rednet.host("ccftp", hostname)
-
-
-
-    while true do
-        local file = textutils.unserialise(awaitFile())
-        local filename = file["filename"]
-        local fileTable = file["file"]
-        local write = ""
-        for _, line in ipairs(fileTable) do
-            write = write .. line .. "\n"
-        end
-        write = string.sub(write, 1, #write - 1)
-        local writeFile = io.open(filename, "w")
-        if writeFile then    
-            writeFile:write(write)
-            writeFile:close()
-        end
-    end
+end
